a0dde7628a770ffe87048ef30cea178b8f6f3319
YoungMin Choi <83854046+richrookie@users.noreply.github.com>
2023-12-20 23:59:27 +0900

    [Bronze V] Title: 개, Time: 56 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,17 @@
+using System;
+using static System.Console;
+
+namespace algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("|\\_/|");
+            WriteLine("|q p|   /}");
+            WriteLine("( 0 )\"\"\"\\");
+            WriteLine("|\"^\"`    |");
+            WriteLine("||_/=\\\\__|");
+        }
+    }
+}
