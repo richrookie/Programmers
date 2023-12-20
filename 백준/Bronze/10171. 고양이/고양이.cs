42411370161c8d1d293eea154111ffc927603acf
YoungMin Choi <83854046+richrookie@users.noreply.github.com>
2023-12-20 23:42:43 +0900

    [Bronze V] Title: 고양이, Time: 56 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,16 @@
+using System;
+using static System.Console;
+
+namespace algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("\\    /\\");
+            WriteLine(" )  ( ')");
+            WriteLine("(  /  )");
+            WriteLine(" \\(__)|");
+        }
+    }
+}
