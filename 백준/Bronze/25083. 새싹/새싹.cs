0ae988a295e0f75a5e2295cf3325ac993b1b488a
YoungMin Choi <83854046+richrookie@users.noreply.github.com>
2023-12-23 20:25:51 +0900

    [Bronze V] Title: 새싹, Time: 56 ms, Memory: 5332 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,18 @@
+using System;
+using static System.Console;
+
+namespace algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("         ,r\'\"7");
+            WriteLine("r`-_   ,\'  ,/");
+            WriteLine(" \\. \". L_r'");
+            WriteLine("   `~\\/");
+            WriteLine("      |");
+            WriteLine("      |");
+        }
+    }
+}
