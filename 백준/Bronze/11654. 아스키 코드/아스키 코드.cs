26e3b024094eaecb351bd7858e650e7f03512620
YoungMin Choi <83854046+richrookie@users.noreply.github.com>
2023-12-23 20:13:04 +0900

    [Bronze V] Title: 아스키 코드, Time: 64 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,15 @@
+using System;
+using static System.Console;
+
+namespace algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = Convert.ToChar(ReadLine());
+            int x = Convert.ToInt32(a);
+            WriteLine(x);
+        }
+    }
+}
